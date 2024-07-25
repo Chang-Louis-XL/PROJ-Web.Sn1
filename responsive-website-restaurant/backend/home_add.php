f934be49e63be04616bebcc08f13b594e005cd2d
Chang Louis <louisvictory0226@gmail.com>
2024-07-25 23:54:57 +0800

    re

--- responsive-website-restaurant/backend/home_add.php
+++ responsive-website-restaurant/backend/home_add.php
@@ -1,3 +1,6 @@
+
+<!-- 此頁未使用 -->
+
 <div class="modal fade" id="addModal" tabindex="-1" aria-labelledby="addModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
