2cf891e8c33aea422105e334f28656953dd8279f
Chang Louis <louisvictory0226@gmail.com>
2024-07-25 22:32:57 +0800

    更新PRO 編輯

--- responsive-website-restaurant/backend/product.php
+++ responsive-website-restaurant/backend/product.php
@@ -4,69 +4,69 @@
     <button type="button" class="btn btn-primary d-flex justify-content-start mb-2" id="addButton">新增圖片</button>
     <div id="modal"></div>
 
-    
-        <table class="table">
-            <thead class="table-light">
+
+    <table class="table">
+        <thead class="table-light">
+            <tr>
+                <th>圖片</th>
+                <th>內容</th>
+                <th>操作</th>
+
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $rows = ${$do}->all();
+            foreach ($rows as $row) {
+                ?>
                 <tr>
-                    <th>圖片</th>
-                    <th>內容</th>
-                    <th>操作</th>
+                    <td class="text-center align-middle">
+                        <img src="<?php echo isset($row['img']) && !empty($row['img']) ? './assets/img/' . $row['img'] : 'https://via.placeholder.com/150'; ?>"
+                            alt="Placeholder Image" class="rounded" style='width:150px;heightgit:150px'>
+                    </td>
+
+                    <td class="text-center align-middle">
+                        <label for="textInput1" class="form-label">menu_name</label>
+                        <input type="text" name="menu_name[]" value="<?= $row['menu_name']; ?>" class="form-control"
+                            id="textInput1" placeholder="內容(1)">
+                        <label for="textInput2" class="form-label">menu_detail</label>
+                        <input type="text" name="menu_detail[]" value="<?= $row['menu_detail']; ?>" class="form-control"
+                            id="textInput2" placeholder="內容(2)">
+                        <label for="textInput2" class="form-label">enu_preci</label>
+                        <input type="text" name="menu_preci[]" value="<?= $row['menu_preci']; ?>" class="form-control"
+                            id="textInput2" placeholder="內容(3)">
+                    </td>
+                    <td class="text-center align-middle">
+                        <input type="hidden" name="id[]" value="<?= $row['id']; ?>">
+                        <input type="checkbox" aria-label="Radio button for following text input" name="sh[]"
+                            value="<?= $row['id']; ?>" <?= ($row['sh'] == 1) ? "checked" : ""; ?>>顯示
+                        <button type="button" class="btn btn-warning"
+                            onclick="editRow(<?= htmlspecialchars(json_encode($row)); ?>)">Edit</button>
+                        <button type="button" class="btn btn-danger"
+                            onclick=" del('Product',<?= $row['id']; ?>)">Del</button>
+                    </td>
 
                 </tr>
-            </thead>
-            <tbody>
-                <?php
-                $rows = ${$do}->all();
-                foreach ($rows as $row) {
-                    ?>
-                    <tr>
-                        <td class="text-center align-middle">
-                            <img src="<?php echo isset($row['img']) && !empty($row['img']) ? './assets/img/' . $row['img'] : 'https://via.placeholder.com/300'; ?>"
-                                alt="Placeholder Image" class="rounded" style='width:150px;heightgit:150px'>
-                        </td>
-
-                        <td class="text-center align-middle">
-                            <label for="textInput1" class="form-label">menu_name</label>
-                            <input type="text" name="menu_name[]" value="<?= $row['menu_name']; ?>" class="form-control"
-                                id="textInput1" placeholder="內容(1)">
-                            <label for="textInput2" class="form-label">menu_detail</label>
-                            <input type="text" name="menu_detail[]" value="<?= $row['menu_detail']; ?>" class="form-control"
-                                id="textInput2" placeholder="內容(2)">
-                            <label for="textInput2" class="form-label">enu_preci</label>
-                            <input type="text" name="menu_preci[]" value="<?= $row['menu_preci']; ?>" class="form-control"
-                                id="textInput2" placeholder="內容(3)">
-                        </td>
-
-                        <td class="text-center align-middle">
-                            <input type="hidden" name="id[]" value="<?= $row['id']; ?>">
-                            <input type="checkbox" name="sh" value="<?= $row['id']; ?>" <?= ($row['sh'] == 1) ? "checked" : ""; ?>>顯示
-                            <button type="button" class="btn btn-warning"  onclick="editRow(<?= json_encode($row); ?>)">Edit</button>
-                            <button type="button" class="btn btn-danger" onclick=" del('Product',<?= $row['id']; ?>)">Del</button>
-                        </td>
-
-                    </tr>
-                <?php } ?>
-            </tbody>
-        </table>
+            <?php } ?>
+        </tbody>
+    </table>
 </div>
 
 
 <script>
 
 
+    function editRow(rowData) {
+        console.log(rowData);
+        const params = new URLSearchParams(rowData).toString();
+        console.log(rowData);
+        const url = './backend/product_add.php?do=Product&' + params;
+        console.log(URL);
+        $('#modal').load(url);
+
+    }
 
 
-function editRow(rowData) {
-    console.log(rowData);  // 確認 rowData 正確
-    const params = new URLSearchParams(rowData).toString();
-    const url = './backend/product_add.php?do=Product' + params;
-    console.log(url);  // 確認 URL 正確
-    $('#modal').load(url, function(response, status, xhr) {
-        if (status == "error") {
-            console.log("Error: " + xhr.status + " " + xhr.statusText);
-        }
-    });
-}
 
 
     document.getElementById('addButton').addEventListener('click', () => {
--- responsive-website-restaurant/backend/product_add.php
+++ responsive-website-restaurant/backend/product_add.php
@@ -1,56 +1,101 @@
-<div class="modal fade" id="addModal" tabindex="-1" aria-labelledby="addModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="w-100 text-center">新增/編輯</h1>
+<style>
+    .sd {
+        background-image: url(../assets/img/03B02.png);
+    }
+</style>
 
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form class='form-group mx-auto col-6 mt-5' action="./api/add.php" method="post"
-                    enctype="multipart/form-data">
+<form class='form-group mx-auto col-6 mt-5' action="./api/add.php" method="post" enctype="multipart/form-data">
+    <div class="modal fade" id="addModal" tabindex="-1" aria-labelledby="addModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="w-100 text-center">新增/編輯</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+
+                <div class="modal-body">
 
 
-                    <div class='d-flex my-2'>
-                        <label for="img" class='col-3 form-label'>圖片</label>
-                        <?php if (isset($_GET['id']) && !empty($_GET['img'])): ?>
-                            <div>
-                                <img src="../assets/img/<?php echo htmlspecialchars($_GET['img']); ?>" alt="Uploaded Image"
-                                    style="max-width: 200px; max-height: 200px;">
-                            </div>
-                        <?php else: ?>
-                            <input type="file" name="img" id="img">
-                        <?php endif; ?>
-                    </div>
+                    <?php if ((isset($_GET['id']) && !empty($_GET['id']))) { ?>
 
-                    <div class='d-flex my-2'>
-                        <label for="" class='col-3 form-label'>menu_name:</label>
-                        <input type="text" name="menu_name"
-                            value="<?php echo (isset($_GET['id']) && !empty($_GET['id'])) ? $_GET['menu_name'] : ''; ?>">
-                    </div>
-                    <div class='d-flex my-2'>
-                        <label for="" class='col-3 form-label'>menu_detail:</label>
-                        <input type="text" name="menu_detail"
-                            value="<?php echo (isset($_GET['id']) && !empty($_GET['id'])) ? $_GET['menu_detail'] : ''; ?>">
-                    </div>
-                    <div class='d-flex my-2'>
-                        <label for="" class='col-3 form-label'>menu_preci:</label>
-                        <input type="text" name="menu_preci"
-                            value="<?php echo (isset($_GET['id']) && !empty($_GET['id'])) ? $_GET['menu_preci'] : ''; ?>">
-                    </div>
+                        <img src="<?php echo isset($row['img']) && !empty($row['img']) ? '../assets/img/' . $row['img'] : 'https://via.placeholder.com/150'; ?>"
+                            alt="Placeholder Image" class="rounded" style='width:150px;heightgit:150px'>
+
+                    <?php } else { ?>
+
+                        <div class='d-flex my-2'>
+                            <label for="" class='col-3 form-label'>圖片</label>
+                            <input type="file" name="img">
+                        </div>
+
+                    <?php } ?>
+
+                </div>
+
+
+                <div class='d-flex my-2'>
+                    <label for="" class='col-3 form-label'>menu_name:</label>
+                    <input type="text" name="menu_name"
+                        value="<?php echo (isset($_GET['id']) && !empty($_GET['id'])) ? $_GET['menu_name'] : ''; ?>">
+                </div>
+                <div class='d-flex my-2'>
+                    <label for="" class='col-3 form-label'>menu_detail:</label>
+                    <input type="text" name="menu_detail"
+                        value="<?php echo (isset($_GET['id']) && !empty($_GET['id'])) ? $_GET['menu_detail'] : ''; ?>">
+                </div>
+                <div class='d-flex my-2'>
+                    <label for="" class='col-3 form-label'>menu_preci:</label>
+                    <input type="text" name="menu_preci"
+                        value="<?php echo (isset($_GET['id']) && !empty($_GET['id'])) ? $_GET['menu_preci'] : ''; ?>">
+                </div>
+
+                <div>
                     <?php if (isset($_GET['id']) && !empty($_GET['id'])) { ?>
                         <input type="hidden" name="id" value="<?= $_GET['id']; ?>">
                     <?php } ?>
-                    <?php if (isset($_GET['sh']) && !empty($_GET['sh'])) { ?>
+
+                    <?php if (isset($_GET['id']) && !empty($_GET['id'])) { ?>
                         <input type="radio" name="sh" value="<?= $_GET['sh']; ?>">
                     <?php } ?>
+
                     <input type="hidden" name="table" value="<?= $do = $_GET['do'] ?? 'Product'; ?>">
                     <input type="submit" value="送出" class='btn btn-primary'>
                     <input type="reset" value="重置" class='btn btn-warning'>
+
+
+                </div>
             </div>
-            </form>
         </div>
     </div>
-</div>
-</div>
+</form>
 
+
+<script>
+
+
+    <?php if (isset($_GET['id']) && !empty($_GET['id'])) { ?>
+
+        // edit啟用
+        function showModal() {
+            // 獲取模態框元素
+            const modalElement = document.querySelector("#addModal");
+
+            // 創建新的模態框實例
+            const addModal = new bootstrap.Modal(modalElement);
+
+            // 添加模態框關閉事件監聽器
+            modalElement.addEventListener('hidden.bs.modal', event => {
+                // 清空模態框內容
+                $("#modal").html("");
+            }, { once: true }); // 使用 { once: true } 確保事件監聽器僅運行一次
+
+            // 顯示模態框
+            addModal.show();
+        }
+
+        // 調用 showModal 函數顯示模態框
+        showModal();
+
+    <?php } ?>
+
+</script>
